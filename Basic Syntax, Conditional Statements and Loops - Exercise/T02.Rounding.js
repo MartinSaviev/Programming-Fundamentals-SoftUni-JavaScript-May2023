5ee7a00a49b341bfd49d42e15b93d9bd5bd7de34
Martin Saviev <114023121+MartinSaviev@users.noreply.github.com>
2023-05-08 14:43:28 +0300

    Revert "Basic Syntax, Conditional Statements and Loops - Exercise"
    This reverts commit 515f771dae46dc0b93f46020970ee6297a157279.

--- Basic Syntax, Conditional Statements and Loops - Exercise/T02.Rounding.js
+++
@@ -1,9 +0,0 @@
-function rounding(number, precision) {
-
-    if (precision > 15) {
-        precision = 15;
-    }
-
-    let fixNumber = number.toFixed(precision)
-    console.log(parseFloat(fixNumber));
-}
